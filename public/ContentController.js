aded5eac1914cd8510b1c521e84f8c2e5837a0ad
Ruud van der Linden <serieuzepost@gmail.com>
2017-11-10 17:12:53 +0100

    Product order popup

---
+++ public/ContentController.js
@@ -0,0 +1,30 @@
+'use strict';
+
+(function () {
+    angular
+        .module('webcart')
+        .controller('ContentCtrl', ContentCtrl);
+
+    function ContentCtrl ($scope, $rootScope, restLocation, session, $http, $timeout) {
+        $scope.messages = {};
+
+        $http.get(restLocation + '/cart/' + session.loggedin_user_id + '/size').then(
+            function(data) {$scope.cart_size = data.data});
+
+        $rootScope.$on('product ordered', function(event, args) {
+            // Add temporary message
+            var timestamp = Date.now();
+
+            $scope.messages[timestamp] = 'Product ' + args['product']['name'] + ' added to cart';
+
+            $timeout(function () {
+                delete $scope.messages[timestamp];
+            }, 3000);
+
+            // Add 1 to cart
+            $scope.cart_size++
+
+        });
+}
+})();
+
--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@
 (function() {
     angular
         .module('webcart', [])
-        .run(function ($rootScope) {
+        .run(function () {
         })
 
         .config(function () {
